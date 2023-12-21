895c3e5849f0ead3a8cdc0b7ba0a45479aae0d7d
Mathias Cabrera <mathiasdesign1@gmail.com>
2023-12-21 19:22:20 -0300

    Parte 6 - Creando Clases Complejas

--- SuperHeroesApp/Program.cs
+++ SuperHeroesApp/Program.cs
@@ -1,31 +1,80 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-var superman = new SuperHeroe();
+var poderVolar = new SuperPoder();
+poderVolar.Nombre = "Volar";
+poderVolar.Descripcion = "Capacidad para volar y planear en el aire";
+poderVolar.Nivel = NivelPoder.NivelDos;
 
+
+var superFuerza = new SuperPoder();
+poderVolar.Nombre = "Super fuerza";
+poderVolar.Nivel = NivelPoder.NivelTres;
+
+
+var poderMoverseVelLuz = new SuperPoder();
+poderMoverseVelLuz.Nombre = "Velocidad luz";
+poderMoverseVelLuz.Descripcion = "Moverse a la velocidad de la luz";
+poderMoverseVelLuz.Nivel = NivelPoder.NivelTres;
+
+
+var poderViajeTiempo = new SuperPoder();
+poderViajeTiempo.Nombre = "Viajar en el tiempo";
+poderViajeTiempo.Nivel = NivelPoder.NivelTres;
+
+
+
+var superman = new SuperHeroe();
 superman.Id = 1;
 superman.Nombre = "SuperMan";
 superman.IdentidadSecreta = "Clark Kent";
 superman.Ciudad = "Metropolis";
 superman.PuedeVolar = true;
-superman.SuperPoderes = new[] {"Vision de rayos X","Volar","Super fuerza","Ojos rayos láser"};
+
+List<SuperPoder> poderesSuperman = new List<SuperPoder>();
+poderesSuperman.Add(poderVolar);
+poderesSuperman.Add(superFuerza);
+
+superman.SuperPoderes = poderesSuperman;
+
+
 
 var flash = new SuperHeroe();
 flash.Id = 1;
 flash.Nombre = "Flash";
 flash.IdentidadSecreta = "Jay Garrick";
 flash.Ciudad = "Central City";
-flash.SuperPoderes = new[] {"Moverse a velocidad luz","Resistencia","Viajar en el tiempo"};
 flash.PuedeVolar = false;
+
+List<SuperPoder> poderesFlash = new List<SuperPoder>();
+poderesFlash.Add(poderMoverseVelLuz);
+poderesFlash.Add(poderViajeTiempo);
+
+flash.SuperPoderes = poderesFlash;
 
 
 //Las Clases deben nombrarse en singular
 class SuperHeroe
 {
+    //public Guid Id;
     public int Id;
     public string Nombre;
     public string IdentidadSecreta;
     public string Ciudad;
-    public string[] SuperPoderes;
+    public List<SuperPoder> SuperPoderes;
     public bool PuedeVolar;
+}
+
+class SuperPoder
+{
+    public string Nombre;
+    public string Descripcion;
+    public NivelPoder Nivel;
+}
+
+enum NivelPoder
+{
+    NivelUno,
+    NivelDos,
+    NivelTres
 }
